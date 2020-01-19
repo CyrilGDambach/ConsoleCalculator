58869b1644d9639bf64f4cdbb60b665ebe5e5116
Cyril <cyrildambach@gmail.com>
2020-01-19 19:58:39 +0100

    Prepare test for subtraction

--- src/main/java/ch/bbw/cd/consolecalculator/Calculator.java
+++ src/main/java/ch/bbw/cd/consolecalculator/Calculator.java
@@ -1,11 +1,11 @@
 package ch.bbw.cd.consolecalculator;
 
-/**
- * Hello world!
- *
- */
 public class Calculator {
 	public int summe(int s1, int s2) {
 		return s1 + s2;
+	}
+	
+	public int differenz(int v1, int v2) {
+		return 0;
 	}
 }
