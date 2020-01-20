67d5953525c1689dd65bc30c9a213b6cdcb44bce
Cyril <cyrildambach@gmail.com>
2020-01-20 22:41:29 +0100

    Calculator has to be fixed

--- src/main/java/ch/bbw/cd/consolecalculator/Calculator.java
+++ src/main/java/ch/bbw/cd/consolecalculator/Calculator.java
@@ -2,9 +2,14 @@ package ch.bbw.cd.consolecalculator;
 
 public class Calculator {
 	public int summe(int s1, int s2) {
+		long result = (long) s1 + (long) s2;
+
+		if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE) {
+			throw new ArithmeticException();
+		}
 		return s1 + s2;
 	}
-	
+
 	public int differenz(int v1, int v2) {
 		return v1 - v2;
 	}
