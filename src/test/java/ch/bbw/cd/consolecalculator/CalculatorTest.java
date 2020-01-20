5716771d5767fd02c98b8e09bd825d33e81ad390
Cyril <cyrildambach@gmail.com>
2020-01-20 22:23:32 +0100

    Summe has to be tested with two negative numbers

--- src/test/java/ch/bbw/cd/consolecalculator/CalculatorTest.java
+++ src/test/java/ch/bbw/cd/consolecalculator/CalculatorTest.java
@@ -6,13 +6,20 @@ import org.junit.Test;
 
 public class CalculatorTest {
 	Calculator testee;
-
+	
+	/*Tests for method Summe*/
 	@Test
 	public void testSummeZweiPositiveIsOk() {
 		testee = new Calculator();
 		assertTrue(testee.summe(10, 25) == 35);
 	}	
+
+	@Test
+	public void test_summe_zweiNegative_OK() {
+		assertTrue(testee.summe(-3, -10) == -13);
+	}
 	
+	/*Tests for method Differenz*/
 	@Test
 	public void testSubtractionZweiPositiveIsOk() {
 		testee = new Calculator();
